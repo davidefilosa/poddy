0fb7e5c9afbb1cfd0916abd28b9455a341499ecc
davide <davideclaypool@gmail.com>
2025-11-03 16:45:35 +0100

    progress bar

--- components/customs/story-detail.tsx
+++ components/customs/story-detail.tsx
@@ -59,9 +59,11 @@ export const StoryDetail = ({ id }: StoryDetailProps) => {
   return (
     <div className="w-full p-2 md:p-8 flex flex-col gap-8 h-screen">
       <motion.div
-        className="fixed top-0 left-0 w-full h-1 bg-black z-50 origin-left"
+        className="fixed top-0 left-0 w-full h-1.5 bg-black z-50 origin-left"
         style={{ scaleX }}
         transition={{ duration: 1 }}
+        initial={{ scaleX: 0 }}
+        animate={{ scaleX }}
       />
       <div className="flex items-center gap-4 justify-between px-2">
         <Button size={"icon-lg"} variant="ghost" onClick={() => router.back()}>
@@ -75,66 +77,68 @@ export const StoryDetail = ({ id }: StoryDetailProps) => {
           </div>
         )}
       </div>
-      <div className="flex flex-col gap-8 w-full">
-        {story.image_url ? (
-          <AspectRatio
-            ratio={16 / 9}
-            className="w-full rounded-lg overflow-hidden"
-          >
-            <Image
-              src={story.image_url}
-              alt="Story Image"
-              fill
-              className="object-cover"
-            />
-          </AspectRatio>
-        ) : (
-          <AspectRatio
-            ratio={16 / 9}
-            className="w-full rounded-lg overflow-hidden"
-          >
-            <Image
-              src={"/placeholder.jpg"}
-              alt="Story Image"
-              fill
-              className="object-cover opacity-50"
-            />
-          </AspectRatio>
-        )}
-      </div>
-      <div className="flex flex-col gap-8 w-full">
-        {story.audio_url ? (
-          <AudioPlayer
-            src={story.audio_url || ""}
-            className="w-full rounded-lg bg-background!"
-          />
-        ) : (
-          <div className="flex flex-col gap-2">
+      <div className="w-full max-w-5xl mx-auto flex flex-col gap-8 md:gap-16">
+        <div className="flex flex-col gap-8 w-full">
+          {story.image_url ? (
+            <AspectRatio
+              ratio={16 / 9}
+              className="w-full rounded-lg overflow-hidden"
+            >
+              <Image
+                src={story.image_url}
+                alt="Story Image"
+                fill
+                className="object-cover"
+              />
+            </AspectRatio>
+          ) : (
+            <AspectRatio
+              ratio={16 / 9}
+              className="w-full rounded-lg overflow-hidden"
+            >
+              <Image
+                src={"/placeholder.jpg"}
+                alt="Story Image"
+                fill
+                className="object-cover opacity-50"
+              />
+            </AspectRatio>
+          )}
+        </div>
+        <div className="flex flex-col gap-8 w-full">
+          {story.audio_url ? (
             <AudioPlayer
               src={story.audio_url || ""}
               className="w-full rounded-lg bg-background!"
             />
-            <div className="text-xs text-muted-foreground px-2 animate-pulse">
-              Audio is being generated
-            </div>
-          </div>
-        )}
-        {story.transcript ? (
-          <ScrollArea className="h-screen p-4 border rounded-lg">
-            <div className="whitespace-pre-wrap text-base md:text-lg">
-              <Markdown>{story.transcript}</Markdown>
-            </div>
-          </ScrollArea>
-        ) : (
-          <div className="flex items-center gap-2 border rounded-lg h-82 justify-center">
-            <div className="flex flex-col items-center gap-2">
-              <Loader2Icon className="w-5 h-5 animate-spin" />
-              <div className="text-xs text-muted-foreground">
-                Content is being generated
+          ) : (
+            <div className="flex flex-col gap-2">
+              <AudioPlayer
+                src={story.audio_url || ""}
+                className="w-full rounded-lg bg-background!"
+              />
+              <div className="text-xs text-muted-foreground px-2 animate-pulse">
+                Audio is being generated
               </div>
             </div>
-          </div>
-        )}
+          )}
+          {story.transcript ? (
+            <ScrollArea className="h-screen md:h-auto p-4 border rounded-lg">
+              <div className="whitespace-pre-wrap text-base md:text-lg">
+                <Markdown>{story.transcript}</Markdown>
+              </div>
+            </ScrollArea>
+          ) : (
+            <div className="flex items-center gap-2 border rounded-lg h-82 justify-center">
+              <div className="flex flex-col items-center gap-2">
+                <Loader2Icon className="w-5 h-5 animate-spin" />
+                <div className="text-xs text-muted-foreground">
+                  Content is being generated
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
