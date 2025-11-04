d52dea48ba870628bec8b40f4ca5ceb023d45fc4
davide <davideclaypool@gmail.com>
2025-11-04 15:35:30 +0100

    add button

--- components/customs/open-button.tsx
+++ components/customs/open-button.tsx
@@ -12,34 +12,43 @@ import {
 import { AspectRatio } from "../ui/aspect-ratio";
 import Image from "next/image";
 import { PlusCircleIcon } from "lucide-react";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Button } from "../ui/button";
 
 export const OpenButton = () => {
   const { setOpen } = useModalStore();
+  const isMobile = useIsMobile();
 
   return (
-    <Card
-      className="w-full cursor-pointer h-full relative group"
-      onClick={() => setOpen(true)}
-    >
-      <div className="absolute top-0 left-0 h-full w-full z-50 flex items-center justify-center opacity-0 hover:opacity-50 transition-opacity">
-        <PlusCircleIcon className="size-50" />
-      </div>
-      <CardHeader>
-        <CardTitle>Create Story</CardTitle>
-        <CardDescription className="line-clamp-4">
-          Click to create a new story based on your chosen topic.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <AspectRatio className="relative w-full" ratio={16 / 9}>
-          <Image
-            src="/story.jpg"
-            alt="Create Story"
-            fill
-            className="object-cover rounded-md"
-          />
-        </AspectRatio>
-      </CardContent>
-    </Card>
+    <>
+      {isMobile ? (
+        <div className="fixed bottom-6 right-6 z-50">
+          <Button
+            size={"icon-lg"}
+            onClick={() => setOpen(true)}
+            className="rounded-full shadow-lg"
+          >
+            <PlusCircleIcon />
+          </Button>
+        </div>
+      ) : (
+        <Card
+          className="w-full cursor-pointer h-full relative group"
+          onClick={() => setOpen(true)}
+        >
+          <CardHeader>
+            <CardTitle>Create Story</CardTitle>
+            <CardDescription className="line-clamp-2">
+              Click to create a new story based on your chosen topic.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="flex items-center justify-center opacity-50 hover:opacity-100 transition-opacity">
+              <PlusCircleIcon className="size-50" />
+            </div>
+          </CardContent>
+        </Card>
+      )}
+    </>
   );
 };
