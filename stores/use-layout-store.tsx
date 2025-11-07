fcda79df8747a6bbdcdb396ca2affcc3b883e2bb
davide <davideclaypool@gmail.com>
2025-11-07 16:15:19 +0100

    local storage

--- stores/use-layout-store.tsx
+++ stores/use-layout-store.tsx
@@ -1,11 +1,20 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface LayoutState {
   layout: "grid" | "list";
   setLayout: (layout: "grid" | "list") => void;
 }
 
-export const useLayoutStore = create<LayoutState>((set) => ({
-  layout: "grid",
-  setLayout: (layout) => set(() => ({ layout })),
-}));
+export const useLayoutStore = create<LayoutState>()(
+  persist(
+    (set) => ({
+      layout: "grid",
+      setLayout: (layout) => set(() => ({ layout })),
+    }),
+    {
+      name: "layout-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
