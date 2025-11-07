bcd68e438f9e1117d7c5d7bb9086bfc7b8a4114b
davide <davideclaypool@gmail.com>
2025-11-07 16:02:44 +0100

    scroll touch

--- components/providers/lenis-provider.tsx
+++ components/providers/lenis-provider.tsx
@@ -12,6 +12,7 @@ export const LenisProvider = ({ children }: { children: React.ReactNode }) => {
         lerp: 0.1,
         duration: 2.5,
         smoothWheel: true,
+        touchMultiplier: 2,
       }}
     >
       {children}
