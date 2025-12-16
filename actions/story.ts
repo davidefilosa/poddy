6aba41175f3f6b76491531157a5e280480e8c41d
Davide Filosa <52102667+davidefilosa@users.noreply.github.com>
2025-12-16 22:13:51 +0100

    Update model version in generateStory function

--- actions/story.ts
+++ actions/story.ts
@@ -11,7 +11,7 @@ import { prismadb } from "@/lib/prismadb";
 async function generateStory(topic: string) {
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.5-pro-preview-03-25",
+    model: "gemini-3-pro-preview"
     generationConfig: {
       responseMimeType: "application/json",
     },
